3963c21f8cfd031be8d1dc07a59cf9315aef4e96
Mario Arita <marioarita502@gmail.com>
2024-08-27 23:49:41 -0600

    feat(routes): :sparkles: New Routes

---
+++ src/app/events-manager/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function EventsManagerPage() {
+  return (
+    <div className='flex items-center justify-center w-full h-screen'>
+        <Link href="/" className="p-4 text-lg font-bold text-blue-500 dark:text-blue-300 lg:text-xl">
+          ← Regresar
+      </Link>
+      EventsManagerPage
+      </div>
+  )
+}
